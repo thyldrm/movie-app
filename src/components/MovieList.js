2fdd1c154105ddec7682a34f3c8c97cff5a3b315
Taha Yıldırım <thyldrm23@gmail.com>
2021-05-01 18:25:07 +0300

    components created

---
+++ src/components/MovieList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class MovieList extends Component {
+    render() {
+        return (
+            <div className="row">
+                <div className="col-lg-4">
+                    <div className="card mb-4 shadow-sm">
+                        <img src="https://www.themoviedb.org/t/p/original/gk1IvgCYg6v5H5cps21ILSOrYER.jpg" className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">
+                                Movie App
+                            </h5>
+                            <p className="card-text">
+                                Movie Desc
+                            </p>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <button type="button" className="btn btn-md btn-outline-danger">Delete</button>
+                                <h2>
+                                    <span className="badge badge-info">8.2</span>
+                                </h2>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MovieList;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+                Search Bar
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
