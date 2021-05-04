07dc47b98a942970f6bedd3a518d01cb56a0934e
Taha Yıldırım <thyldrm23@gmail.com>
2021-05-04 14:31:40 +0300

    delete request updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import MovieList from "./MovieList";
+import axios from "axios";
 
 class App extends React.Component {
 
@@ -10,13 +11,14 @@ class App extends React.Component {
     }
 
     async componentDidMount(){
-        const baseURL = "http://localhost:3002/movies";
-        const response = await fetch(baseURL);
-        const data= await response.json();
-        this.setState({movies: data})
+        const response = await axios.get("http://localhost:3002/movies")
+        this.setState({movies: response.data})
     }
 
     deleteMovie = (movie) => {
+
+        axios.delete(`http://localhost:3002/movies/${movie.id}`)
+
         const newMovieList = this.state.movies.filter(m => m.id !== movie.id);
 
         this.setState(state =>({
